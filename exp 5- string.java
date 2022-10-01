c873bd06d81d1e19fce47eb9e3cf2dafccb080ca
vinushaF <112194872+vinushaF@users.noreply.github.com>
2022-10-01 19:51:29 +0530

    Create exp 5- string.java

---
+++ exp 5- string.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class StringOperator
+{
+   public static void main(String args[])
+   {
+       String str1, str2;
+       Scanner scan = new Scanner(System.in);
+ 
+       System.out.print("Enter a String : ");
+       str1 = scan.nextLine();
+ 
+       str2 = str1.replaceAll("[aeiouAEIOU]", "");
+ 
+ 
+       System.out.print(str2);
+   }
+}
